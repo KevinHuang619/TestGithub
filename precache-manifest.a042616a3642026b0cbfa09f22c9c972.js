9dd82d0ece201fb1222363e57d1e6845b08b0b7e
Kevin <apple163995@gmail.com>
2019-09-11 23:16:22 +0800

    Updates

---
+++ precache-manifest.a042616a3642026b0cbfa09f22c9c972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177c56e05fd3c26f5a1bbb516676b711",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/index.html"
+  },
+  {
+    "revision": "e978530111069b3a38e6",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0c84bea58b18a99b069c",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/static/js/2.0af1d0cc.chunk.js"
+  },
+  {
+    "revision": "e978530111069b3a38e6",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/static/js/main.a6042bd5.chunk.js"
+  },
+  {
+    "revision": "13084a933ac9c5d41de2",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/static/js/runtime~main.35faadd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/static/media/logo.5d5d9eef.svg"
+  }
+]);
