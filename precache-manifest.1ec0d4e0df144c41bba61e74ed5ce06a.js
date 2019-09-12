4ca07dbf8ff9b5aa37467e7f9f0109833a38f3ac
Kevin <apple163995@gmail.com>
2019-09-12 09:59:44 +0800

    Updates

--- precache-manifest.1ec0d4e0df144c41bba61e74ed5ce06a.js
+++ precache-manifest.1ec0d4e0df144c41bba61e74ed5ce06a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "177c56e05fd3c26f5a1bbb516676b711",
+    "revision": "fddaf798858912635f9ff248921a7c89",
     "url": "//WAC-Kevinboy.github.io/TestGithub/index.html"
   },
   {
-    "revision": "e978530111069b3a38e6",
+    "revision": "54d2ce585538ad8698e3",
     "url": "//WAC-Kevinboy.github.io/TestGithub/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "//WAC-Kevinboy.github.io/TestGithub/static/js/2.0af1d0cc.chunk.js"
   },
   {
-    "revision": "e978530111069b3a38e6",
-    "url": "//WAC-Kevinboy.github.io/TestGithub/static/js/main.a6042bd5.chunk.js"
+    "revision": "54d2ce585538ad8698e3",
+    "url": "//WAC-Kevinboy.github.io/TestGithub/static/js/main.31505ed3.chunk.js"
   },
   {
     "revision": "13084a933ac9c5d41de2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//WAC-Kevinboy.github.io/TestGithub/precache-manifest.a042616a3642026b0cbfa09f22c9c972.js"
+  "//WAC-Kevinboy.github.io/TestGithub/precache-manifest.1ec0d4e0df144c41bba61e74ed5ce06a.js"
 );
 
 self.addEventListener('message', (event) => {
